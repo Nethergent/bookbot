3cea6a642c74f5cc8945f4f17439c6527150012c
nethergent <nethergent@proton.me>
2025-01-13 17:39:28 +0000

    added open function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = open_book_text(book_path)
+    print (text)
+
+def open_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+main()
