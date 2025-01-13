e20ab48a3c169a6b41c6f5f2f97a8d78133ea5c8
nethergent <nethergent@proton.me>
2025-01-13 18:02:09 +0000

    added characters function

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = open_book_text(book_path)
-    words = count_words(text)
-    print(f"{words} words found in the document.")
+    word_count = count_words(text)
+    character_count = count_characters(text)
+
+    print(f"{word_count} words found in the document.")
+    print(f"{character_count}")
 
 def open_book_text(path):
     with open(path) as f:
@@ -11,5 +14,19 @@ def open_book_text(path):
 def count_words(text):
     count = len(text.split())
     return count
+
+def count_characters(text):
+    characters = {}
+    
+    # convert all characters to lowercase, avoiding duplicates
+    text = text.lower()   
+    
+    for c in text:
+        if c not in characters:
+            characters[c] = 1
+
+        else:
+            characters[c] += 1
+    return characters
 
 main()
